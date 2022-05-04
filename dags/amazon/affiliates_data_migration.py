87e40483efd661194d437e6b71a36a969863c643
vinoth <vinoth@gmail.com>
2022-05-04 11:46:43 +0530

    amazon test2 commit 02

--- dags/amazon/affiliates_data_migration.py
+++ dags/amazon/affiliates_data_migration.py
@@ -8,7 +8,7 @@ from plugins.utilities.slack_service import success_alert, failure_alert
 test line to check the conflit error 
 
 # ======================================
-# CONFIGURATION for AMAZON
+# CONFIGURATION for AMAZON test2
 # ======================================
 
 ENV = AppConfig.environment
