c7d53ed6f3ab47f90ca284d9692c95976fe57e77
vinoth <vinoth@gmail.com>
2022-05-04 16:00:42 +0530

    amazon2 commit 01

--- dags/amazon/amazon_affiliate_bronze.py
+++ dags/amazon/amazon_affiliate_bronze.py
@@ -6,7 +6,7 @@ from plugins.config import AmazonAffiliatesConfig as config, AppConfig, VaultCon
 from plugins.utilities.slack_service import success_alert, failure_alert
 
 # ======================================
-# CONFIGURATION
+# CONFIGURATION for amazon
 # ======================================
 
 ENV = AppConfig.environment
